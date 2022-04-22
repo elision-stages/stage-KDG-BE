a76014c33c9a9bd1c65575251a851697e19ab860
BertVanBaelen <48016694+BertVanBaelen@users.noreply.github.com>
2022-04-22 13:51:39 +0200

    added documentation

--- src/main/java/eu/elision/marketplace/web/webexceptions/NotFoundException.java
+++ src/main/java/eu/elision/marketplace/web/webexceptions/NotFoundException.java
@@ -2,6 +2,9 @@ package eu.elision.marketplace.web.webexceptions;
 
 public class NotFoundException extends RuntimeException
 {
+    /**
+     * Exception used when something is not found
+     */
     public NotFoundException(){
         super("not found");
     }
