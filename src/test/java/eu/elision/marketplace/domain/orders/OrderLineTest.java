23d3b54ebdbcd10d28042c4506c415248b99a510
BertVanBaelen <bert.vanbaelen@student.kdg.be>
2022-04-25 14:48:57 +0200

    added more testing

--- src/test/java/eu/elision/marketplace/domain/orders/OrderLineTest.java
+++ src/test/java/eu/elision/marketplace/domain/orders/OrderLineTest.java
@@ -62,4 +62,26 @@ class OrderLineTest {
 
         assertThat(ol.getQuantity()).isEqualTo(1);
     }
+
+    @Test
+    void testToString() {
+        OrderLine ol = new OrderLine();
+        ol.setOrderNumber("1");
+        ol.setQuantity(2);
+        ol.setProduct(new Product(1, new Vendor(), "desc", new ArrayList<>(), new ArrayList<>()));
+        ol.setVendor(new Vendor());
+        ol.setOrderLineNumber(1);
+
+        assertThat(ol.toString()).hasToString("OrderLine(orderLineNumber=1, vendor=eu.elision.marketplace.domain.users.Vendor@325f7fa9, orderNumber=1, product=Product(price=1.0, vendor=eu.elision.marketplace.domain.users.Vendor@1bdbf9be, description=desc, images=[], attributes=[]), quantity=2)");
+    }
+    @Test
+    void testEquals(){
+        Vendor vendor = new Vendor();
+        Product product = new Product();
+        OrderLine ol1 = new OrderLine(1, vendor, "1", product, 1);
+        OrderLine ol2 = new OrderLine(1, vendor, "1", product, 1);
+
+        assertThat(ol1.equals(ol2)).isTrue();
+        ol1.canEqual(ol2);
+    }
 }
