914ccb349f8e1c054975350c3ccb4cebc2854f32
BertVanBaelen <bert.vanbaelen@student.kdg.be>
2022-04-26 15:07:27 +0200

    delombok order orderline and product

--- src/main/java/eu/elision/marketplace/domain/orders/OrderLine.java
+++ src/main/java/eu/elision/marketplace/domain/orders/OrderLine.java
@@ -2,14 +2,10 @@ package eu.elision.marketplace.domain.orders;
 
 import eu.elision.marketplace.domain.product.Product;
 import eu.elision.marketplace.domain.users.Vendor;
-import lombok.*;
 
 /**
  * An orderline contains the product and the quantity and the ordernumber of the order it belongs to.
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class OrderLine
 {
     private int orderLineNumber;
@@ -18,11 +14,104 @@ public class OrderLine
     private Product product;
     private int quantity;
 
+    public OrderLine(int orderLineNumber, Vendor vendor, String orderNumber, Product product, int quantity) {
+        this.orderLineNumber = orderLineNumber;
+        this.vendor = vendor;
+        this.orderNumber = orderNumber;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public OrderLine() {
+    }
+
     /**
      * Get the total price of the order line
      * @return the total price of the order line
      */
     public double getTotalPrice(){
         return product.getPrice() * quantity;
+    }
+
+    public int getOrderLineNumber() {
+        return this.orderLineNumber;
+    }
+
+    public Vendor getVendor() {
+        return this.vendor;
+    }
+
+    public String getOrderNumber() {
+        return this.orderNumber;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setOrderLineNumber(int orderLineNumber) {
+        this.orderLineNumber = orderLineNumber;
+    }
+
+    public void setVendor(Vendor vendor) {
+        this.vendor = vendor;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof OrderLine)) return false;
+        final OrderLine other = (OrderLine) o;
+        if (!other.canEqual((Object) this)) return false;
+        if (this.getOrderLineNumber() != other.getOrderLineNumber()) return false;
+        final Object this$vendor = this.getVendor();
+        final Object other$vendor = other.getVendor();
+        if (this$vendor == null ? other$vendor != null : !this$vendor.equals(other$vendor)) return false;
+        final Object this$orderNumber = this.getOrderNumber();
+        final Object other$orderNumber = other.getOrderNumber();
+        if (this$orderNumber == null ? other$orderNumber != null : !this$orderNumber.equals(other$orderNumber))
+            return false;
+        final Object this$product = this.getProduct();
+        final Object other$product = other.getProduct();
+        if (this$product == null ? other$product != null : !this$product.equals(other$product)) return false;
+        if (this.getQuantity() != other.getQuantity()) return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof OrderLine;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        result = result * PRIME + this.getOrderLineNumber();
+        final Object $vendor = this.getVendor();
+        result = result * PRIME + ($vendor == null ? 43 : $vendor.hashCode());
+        final Object $orderNumber = this.getOrderNumber();
+        result = result * PRIME + ($orderNumber == null ? 43 : $orderNumber.hashCode());
+        final Object $product = this.getProduct();
+        result = result * PRIME + ($product == null ? 43 : $product.hashCode());
+        result = result * PRIME + this.getQuantity();
+        return result;
+    }
+
+    public String toString() {
+        return "OrderLine(orderLineNumber=" + this.getOrderLineNumber() + ", vendor=" + this.getVendor() + ", orderNumber=" + this.getOrderNumber() + ", product=" + this.getProduct() + ", quantity=" + this.getQuantity() + ")";
     }
 }
--- src/main/java/eu/elision/marketplace/domain/product/Product.java
+++ src/main/java/eu/elision/marketplace/domain/product/Product.java
@@ -2,7 +2,6 @@ package eu.elision.marketplace.domain.product;
 
 import eu.elision.marketplace.domain.product.category.attributes.value.DynamicAttributeValue;
 import eu.elision.marketplace.domain.users.Vendor;
-import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,8 +9,6 @@ import java.util.List;
 /**
  * This product contains the info of a product
  */
-@Data
-@AllArgsConstructor
 public class Product
 {
     private double price;
@@ -24,5 +21,100 @@ public class Product
     public Product() {
         this.images = new ArrayList<>();
         this.attributes = new ArrayList<>();
+    }
+
+    public Product(double price, Vendor vendor, String description, List<String> images, List<DynamicAttributeValue<?>> attributes) {
+        this.price = price;
+        this.vendor = vendor;
+        this.description = description;
+        this.images = images;
+        this.attributes = attributes;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public Vendor getVendor() {
+        return this.vendor;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public List<String> getImages() {
+        return this.images;
+    }
+
+    public List<DynamicAttributeValue<?>> getAttributes() {
+        return this.attributes;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setVendor(Vendor vendor) {
+        this.vendor = vendor;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setImages(List<String> images) {
+        this.images = images;
+    }
+
+    public void setAttributes(List<DynamicAttributeValue<?>> attributes) {
+        this.attributes = attributes;
+    }
+
+    public boolean equals(final Object o) {
+        if (o == this) return true;
+        if (!(o instanceof Product)) return false;
+        final Product other = (Product) o;
+        if (!other.canEqual((Object) this)) return false;
+        if (Double.compare(this.getPrice(), other.getPrice()) != 0) return false;
+        final Object this$vendor = this.getVendor();
+        final Object other$vendor = other.getVendor();
+        if (this$vendor == null ? other$vendor != null : !this$vendor.equals(other$vendor)) return false;
+        final Object this$description = this.getDescription();
+        final Object other$description = other.getDescription();
+        if (this$description == null ? other$description != null : !this$description.equals(other$description))
+            return false;
+        final Object this$images = this.getImages();
+        final Object other$images = other.getImages();
+        if (this$images == null ? other$images != null : !this$images.equals(other$images)) return false;
+        final Object this$attributes = this.getAttributes();
+        final Object other$attributes = other.getAttributes();
+        if (this$attributes == null ? other$attributes != null : !this$attributes.equals(other$attributes))
+            return false;
+        return true;
+    }
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof Product;
+    }
+
+    public int hashCode() {
+        final int PRIME = 59;
+        int result = 1;
+        final long $price = Double.doubleToLongBits(this.getPrice());
+        result = result * PRIME + (int) ($price >>> 32 ^ $price);
+        final Object $vendor = this.getVendor();
+        result = result * PRIME + ($vendor == null ? 43 : $vendor.hashCode());
+        final Object $description = this.getDescription();
+        result = result * PRIME + ($description == null ? 43 : $description.hashCode());
+        final Object $images = this.getImages();
+        result = result * PRIME + ($images == null ? 43 : $images.hashCode());
+        final Object $attributes = this.getAttributes();
+        result = result * PRIME + ($attributes == null ? 43 : $attributes.hashCode());
+        return result;
+    }
+
+    public String toString() {
+        return "Product(price=" + this.getPrice() + ", vendor=" + this.getVendor() + ", description=" + this.getDescription() + ", images=" + this.getImages() + ", attributes=" + this.getAttributes() + ")";
     }
 }
