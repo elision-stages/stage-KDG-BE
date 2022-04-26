1a81e40654dd1c9ac6846d7d30156476dd60a1a9
BertVanBaelen <bert.vanbaelen@student.kdg.be>
2022-04-26 16:18:32 +0200

    code cleanup

--- src/main/java/eu/elision/marketplace/MarketplaceApplication.java
+++ src/main/java/eu/elision/marketplace/MarketplaceApplication.java
@@ -2,26 +2,7 @@ package eu.elision.marketplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseBuilder;
-import springfox.documentation.schema.ScalarType;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.ParameterType;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
-
-import java.time.LocalDate;
-
-import static java.util.Collections.singletonList;
-import static springfox.documentation.schema.AlternateTypeRules.newRule;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -30,89 +11,4 @@ public class MarketplaceApplication {
     public static void main(String[] args) {
         SpringApplication.run(MarketplaceApplication.class, args);
     }
-
-    //@Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    //@Bean
-    public Docket marketplaceApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/")
-                .directModelSubstitute(LocalDate.class, String.class)
-                .genericModelSubstitutes(ResponseEntity.class)
-                .useDefaultResponseMessages(false)
-                .globalResponses(HttpMethod.GET,
-                        singletonList(new ResponseBuilder()
-                                .code("500")
-                                .description("500 message")
-                                .representation(MediaType.TEXT_XML)
-                                .apply(r ->
-                                        r.model(m ->
-                                                m.referenceModel(ref ->
-                                                        ref.key(k ->
-                                                                k.qualifiedModelName(q ->
-                                                                        q.namespace("some:namespace")
-                                                                                .name("ERROR"))))))
-                                .build()))
-                .securitySchemes(singletonList(apiKey()))
-                .enableUrlTemplating(true)
-                .globalRequestParameters(
-                        singletonList(new springfox.documentation.builders.RequestParameterBuilder()
-                                .name("someGlobalParameter")
-                                .description("Description of someGlobalParameter")
-                                .in(ParameterType.QUERY)
-                                .required(true)
-                                .query(q -> q.model(m -> m.scalarModel(ScalarType.STRING)))
-                                .build()));
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey("mykey", "api_key", "header");
-    }
-
-    //@Bean
-    SecurityConfiguration security() {
-        return SecurityConfigurationBuilder.builder()
-                .clientId("test-app-client-id")
-                .clientSecret("test-app-client-secret")
-                .realm("test-app-realm")
-                .appName("test-app")
-                .scopeSeparator(",")
-                .additionalQueryStringParams(null)
-                .useBasicAuthenticationWithAccessCodeGrant(false)
-                .enableCsrfSupport(false)
-                .build();
-    }
-
-    //@Bean
-    UiConfiguration uiConfig() {
-        return UiConfigurationBuilder.builder()
-                .deepLinking(true)
-                .displayOperationId(false)
-                .defaultModelsExpandDepth(1)
-                .defaultModelExpandDepth(1)
-                .defaultModelRendering(ModelRendering.EXAMPLE)
-                .displayRequestDuration(false)
-                .docExpansion(DocExpansion.NONE)
-                .filter(false)
-                .maxDisplayedTags(null)
-                .operationsSorter(OperationsSorter.ALPHA)
-                .showExtensions(false)
-                .showCommonExtensions(false)
-                .tagsSorter(TagsSorter.ALPHA)
-                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
-                .validatorUrl(null)
-                .build();
-    }
-
 }
