76465ba2995aca27ea40477ce941151990b4fc63
BertVanBaelen <bert.vanbaelen@student.kdg.be>
2022-04-26 13:42:12 +0200

    swagger not working yet

---
+++ src/main/java/eu/elision/marketplace/config/ApplicationProperties.java
@@ -0,0 +1,18 @@
+package eu.elision.marketplace.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("api")
+public class ApplicationProperties {
+    private String version;
+    private String title;
+    private String basePackage;
+    private String description;
+    private String terms;
+}
