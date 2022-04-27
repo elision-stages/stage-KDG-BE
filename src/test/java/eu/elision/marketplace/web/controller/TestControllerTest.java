7d23821d28b4d2584b13d2f64c55b219464d545a
BertVanBaelen <bert.vanbaelen@student.kdg.be>
2022-04-27 14:53:55 +0200

    fix tests

--- src/test/java/eu/elision/marketplace/web/controller/TestControllerTest.java
+++ src/test/java/eu/elision/marketplace/web/controller/TestControllerTest.java
@@ -1,34 +1,40 @@
 package eu.elision.marketplace.web.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import javax.servlet.http.HttpServletResponse;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class TestControllerTest {
-    private static final String BASE_URL = "http://localhost:";
-    @Autowired
-    private TestRestTemplate testRestTemplate;
+    private static URL base;
+    private TestRestTemplate restTemplate;
     @LocalServerPort
     private Integer port;
 
+    @BeforeEach
+    void setUp() throws MalformedURLException {
+        restTemplate = new TestRestTemplate("user", "password");
+        base = new URL(String.format("http://localhost:%s", port));
+    }
+
     @Test
     void hello() {
-        String response = testRestTemplate.getForObject(String.format("%s%s/hello", BASE_URL, port.toString()), String.class);
+        String response = restTemplate.getForObject(String.format("%s/hello", base), String.class);
         assertThat(response).isEqualTo("hello");
     }
 
     @Test
     void testException() {
-        ResponseEntity<String> respent = testRestTemplate.getForEntity(String.format("%s%s/testException", BASE_URL, port.toString()), String.class);
+        ResponseEntity<String> respent = restTemplate.getForEntity(String.format("%s/testException", base), String.class);
         assertThat(respent.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(respent.getBody()).isEqualTo("not found");
     }
