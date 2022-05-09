6f12c17ab6d1aa64a9dd1bd2ff601243f772555c
vanbabe <104006124+vanbabe@users.noreply.github.com>
2022-05-09 10:17:25 +0200

    remove security hotspot

--- src/main/java/eu/elision/marketplace/web/controller/ProductController.java
+++ src/main/java/eu/elision/marketplace/web/controller/ProductController.java
@@ -8,11 +8,13 @@ import eu.elision.marketplace.web.dtos.ProductDto;
 import eu.elision.marketplace.web.dtos.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class ProductController
 {
