c9a6b2d0f0e97c491ffd99c010434c7657b0b276
Anton Vroemans <anton.vroemans@student.kdg.be>
2022-05-09 14:19:40 +0200

    Added converter test class

---
+++ src/test/java/eu/elision/marketplace/services/converter/ConverterTest.java
@@ -0,0 +1,32 @@
+package eu.elision.marketplace.services.converter;
+
+import eu.elision.marketplace.domain.product.Product;
+import eu.elision.marketplace.domain.users.Vendor;
+import eu.elision.marketplace.web.dtos.AlgoliaProductDto;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class ConverterTest {
+    @Autowired
+    Converter<Product, AlgoliaProductDto> algoliaProductConverter;
+
+    @Test
+    void test() {
+        Vendor vendor = new Vendor();
+        vendor.setBusinessName("vendor");
+        Product product = new Product();
+        product.setId(123L);
+        product.setVendor(vendor);
+        product.setImages(List.of("test"));
+        AlgoliaProductDto result = algoliaProductConverter.convert(product);
+        assertThat(result.getImage()).isEqualTo("test");
+        assertThat(result.getVendor()).isEqualTo("vendor");
+    }
+}
