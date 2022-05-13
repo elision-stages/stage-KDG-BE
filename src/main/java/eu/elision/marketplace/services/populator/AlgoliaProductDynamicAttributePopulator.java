df4b13f8a7b0ea82be392ef2ded55020993dc96f
Anton Vroemans <anton.vroemans@student.kdg.be>
2022-05-13 11:56:26 +0200

    Resolved review comments

--- src/main/java/eu/elision/marketplace/services/populator/AlgoliaProductDynamicAttributePopulator.java
+++ src/main/java/eu/elision/marketplace/services/populator/AlgoliaProductDynamicAttributePopulator.java
@@ -1,6 +1,7 @@
 package eu.elision.marketplace.services.populator;
 
 import eu.elision.marketplace.domain.product.Product;
+import eu.elision.marketplace.domain.product.category.attributes.value.DynamicAttributeValue;
 import eu.elision.marketplace.web.dtos.AlgoliaProductDto;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,6 @@ public class AlgoliaProductDynamicAttributePopulator implements Populator<Produc
 
     @Override
     public void populate(Product source, AlgoliaProductDto target) {
-        // TODO
+        source.getAttributes().forEach(attr -> target.getParameters().put(attr.getAttributeName(), attr.getValue()));
     }
 }
--- src/test/java/eu/elision/marketplace/services/converter/ConverterTest.java
+++ src/test/java/eu/elision/marketplace/services/converter/ConverterTest.java
@@ -3,18 +3,14 @@ package eu.elision.marketplace.services.converter;
 import eu.elision.marketplace.domain.product.Product;
 import eu.elision.marketplace.domain.users.Vendor;
 import eu.elision.marketplace.web.dtos.AlgoliaProductDto;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 class ConverterTest {
     @Autowired
     Converter<Product, AlgoliaProductDto> algoliaProductConverter;
