494db4052f68042c70cd7d51f358a601b5759592
vanbabe <104006124+vanbabe@users.noreply.github.com>
2022-05-16 01:49:31 +0200

    fix problems

--- src/test/java/eu/elision/marketplace/domain/orders/OrderLineTest.java
+++ src/test/java/eu/elision/marketplace/domain/orders/OrderLineTest.java
@@ -99,8 +99,8 @@ class OrderLineTest
         ol.setVendor(null);
         ol.setOrderLineNumber(orderLineNumber);
 
-        assertThat(ol.toString()).hasToString(String.format("OrderLine(orderLineNumber=%s, vendor=null, orderNumber=%s, product=Product(id=%s, title=%s, price=%s, category=null, vendor=null, description=%s, images=[], attributes=[]), quantity=%s)",
-                orderLineNumber, orderNumber, id, name, price, description, quantity));
+        assertThat(ol.toString()).hasToString(String.format("OrderLine(orderLineNumber=%s, vendor=null, orderNumber=%s, product=Product(id=%s, price=%s, title=%s, category=null, vendor=null, description=%s, images=[], attributes=[]), quantity=%s)",
+                orderLineNumber, orderNumber, id, price, name, description, quantity));
     }
 
     @Test
