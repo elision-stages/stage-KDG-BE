81bdc33f4f6f7e553dcab565693882ab660aea6b
BertVanBaelen <104006124+vanbabe@users.noreply.github.com>
2022-05-19 11:14:39 +0200

    added testing

--- src/test/java/eu/elision/marketplace/logic/ControllerTest.java
+++ src/test/java/eu/elision/marketplace/logic/ControllerTest.java
@@ -7,7 +7,8 @@ import eu.elision.marketplace.domain.product.category.Category;
 import eu.elision.marketplace.domain.users.Address;
 import eu.elision.marketplace.domain.users.Customer;
 import eu.elision.marketplace.domain.users.Vendor;
-import eu.elision.marketplace.repositories.OrderRepository;
+import eu.elision.marketplace.logic.services.orders.OrderService;
+import eu.elision.marketplace.logic.services.users.UserService;
 import eu.elision.marketplace.web.dtos.cart.AddProductToCartDto;
 import eu.elision.marketplace.web.dtos.cart.CartDto;
 import eu.elision.marketplace.web.dtos.cart.OrderLineDto;
@@ -17,39 +18,30 @@ import eu.elision.marketplace.web.dtos.product.ProductDto;
 import eu.elision.marketplace.web.dtos.users.CustomerDto;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Locale;
-import java.util.Objects;
+import java.time.LocalDate;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 @SpringBootTest
-class ControllerTest {
+class ControllerTest
+{
 
-
-    @InjectMocks
     @Autowired
     Controller controller;
-    @Mock
-    OrderRepository orderRepository;
-
-    @BeforeEach
-    public void init() {
-        MockitoAnnotations.openMocks(this);
-    }
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    UserService userService;
 
     @Test
-    void saveCostumerWithAddress() {
+    void saveCostumerWithAddress()
+    {
         final int initUserRepoSize = controller.findAllUsers().size();
         final int initAddressRepoSize = controller.findAllAddresses().size();
 
@@ -99,7 +91,8 @@ class ControllerTest {
     }
 
     @Test
-    void saveCustomerWithoutAddress() {
+    void saveCustomerWithoutAddress()
+    {
         final int initUserRepoSize = controller.findAllUsers().size();
 
         final Customer customer = new Customer();
@@ -128,12 +121,14 @@ class ControllerTest {
     }
 
     @Test
-    void findAllCategoriesTest() {
+    void findAllCategoriesTest()
+    {
         assertThat(controller.findAllCategories()).isNotNull();
     }
 
     @Test
-    void findAllCustomerDtoTest() {
+    void findAllCustomerDtoTest()
+    {
         final int initSize = controller.findAllCustomerDto().size();
 
         controller.saveCustomer(new CustomerDto(RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(5), String.format("%s@%s.%s", RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(2)), String.format("%s%s%s", RandomStringUtils.randomAlphabetic(5).toLowerCase(Locale.ROOT), RandomUtils.nextInt(1, 100), RandomStringUtils.randomAlphabetic(2).toUpperCase(Locale.ROOT))));
@@ -141,7 +136,8 @@ class ControllerTest {
     }
 
     @Test
-    void findProductsByVendor() {
+    void findProductsByVendor()
+    {
         final String firstName = RandomStringUtils.randomAlphabetic(5);
         final String lastName = RandomStringUtils.randomAlphabetic(5);
         final String email = String.format("%s@%s.%s", RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(2));
@@ -168,7 +164,8 @@ class ControllerTest {
     }
 
     @Test
-    void addGetProductToCartTest() {
+    void addGetProductToCartTest()
+    {
         final String firstName = RandomStringUtils.randomAlphabetic(5);
         final String lastName = RandomStringUtils.randomAlphabetic(5);
         final String email = String.format("%s@%s.%s", RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(2));
@@ -217,7 +214,8 @@ class ControllerTest {
     }
 
     @Test
-    void deleteProductTest() {
+    void deleteProductTest()
+    {
         final int initSize = controller.findAllProducts().size();
 
         Product product = new Product();
@@ -229,7 +227,8 @@ class ControllerTest {
     }
 
     @Test
-    void checkoutCartTest() {
+    void checkoutCartTest()
+    {
         final Customer customer = new Customer();
 
         final String firstName = RandomStringUtils.randomAlphabetic(5);
@@ -275,7 +274,8 @@ class ControllerTest {
     }
 
     @Test
-    void getVendorOrders() {
+    void getVendorOrders()
+    {
         Vendor vendor = new Vendor();
         final String firstName = RandomStringUtils.randomAlphabetic(4);
         final String lastName = RandomStringUtils.randomAlphabetic(4);
@@ -324,7 +324,8 @@ class ControllerTest {
     }
 
     @Test
-    void saveProductTest() {
+    void saveProductTest()
+    {
         Vendor vendor = new Vendor();
         final String firstName = RandomStringUtils.randomAlphabetic(4);
         final String lastName = RandomStringUtils.randomAlphabetic(4);
@@ -353,5 +354,49 @@ class ControllerTest {
         assertThat(product.getCategory().getName()).isEqualTo(productDto.category().getName());
 
     }
+
+    @Test
+    void getCustomerOrdersTest()
+    {
+        Customer customer = new Customer();
+        customer.setLastName(RandomStringUtils.randomAlphabetic(5));
+        customer.setFirstName(RandomStringUtils.randomAlphabetic(5));
+        customer.setEmail(String.format("%s@%s.%s", RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(2)));
+        customer.setPassword(String.format("%s%s%s", RandomStringUtils.randomAlphabetic(5).toLowerCase(Locale.ROOT), RandomUtils.nextInt(1, 100), RandomStringUtils.randomAlphabetic(2).toUpperCase(Locale.ROOT)));
+        userService.save(customer);
+
+        Order order = new Order();
+        order.setUser(customer);
+        order = orderService.save(order);
+
+        final Product product = new Product();
+        controller.saveProduct(product);
+
+        final Vendor vendor = new Vendor();
+        vendor.setLastName(RandomStringUtils.randomAlphabetic(5));
+        vendor.setFirstName(RandomStringUtils.randomAlphabetic(5));
+        vendor.setEmail(String.format("%s@%s.%s", RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(5), RandomStringUtils.randomAlphabetic(2)));
+        vendor.setPassword(String.format("%s%s%s", RandomStringUtils.randomAlphabetic(5).toLowerCase(Locale.ROOT), RandomUtils.nextInt(1, 100), RandomStringUtils.randomAlphabetic(2).toUpperCase(Locale.ROOT)));
+        vendor.setPhoneNumber(RandomStringUtils.random(10, false, true));
+        userService.save(vendor);
+
+        order.setLines(List.of(
+                new OrderLine(RandomUtils.nextInt(), vendor, String.valueOf(order.getOrderNumber()), product, 2)
+        ));
+        orderService.save(order);
+
+        Collection<OrderDto> customerOrders = controller.getOrders(customer.getEmail());
+
+        assertThat(customerOrders).hasSize(1);
+
+        final OrderDto orderDto = customerOrders.stream().findFirst().orElse(null);
+        assertThat(orderDto).isNotNull();
+        assertThat(orderDto.getCustomerName()).isEqualTo(customer.getFullName());
+        assertThat(orderDto.getOrderDate()).isEqualTo(LocalDate.now().toString());
+        assertThat(orderDto.getOrderNumber()).isEqualTo(order.getOrderNumber());
+        assertThat(orderDto.getNumberProducts()).isEqualTo(order.getProductCount());
+        assertThat(orderDto.getTotalPrice()).isEqualTo(order.getTotalPrice());
+    }
+
 
 }
