46de10104200c233764376f3380a9556574e5a79
BertVanBaelen <104006124+vanbabe@users.noreply.github.com>
2022-05-23 09:56:20 +0200

    add business name validation and vat validation

--- src/main/java/eu/elision/marketplace/logic/services/users/UserService.java
+++ src/main/java/eu/elision/marketplace/logic/services/users/UserService.java
@@ -4,6 +4,7 @@ import eu.elision.marketplace.domain.users.Customer;
 import eu.elision.marketplace.domain.users.User;
 import eu.elision.marketplace.domain.users.Vendor;
 import eu.elision.marketplace.repositories.UserRepository;
+import eu.elision.marketplace.web.api.vat.VATClient;
 import eu.elision.marketplace.web.dtos.users.CustomerDto;
 import eu.elision.marketplace.web.dtos.users.VendorDto;
 import eu.elision.marketplace.web.webexceptions.InvalidDataException;
@@ -26,9 +27,11 @@ import java.util.Set;
  */
 @Service
 @NoArgsConstructor
-public class UserService implements UserDetailsService {
+public class UserService implements UserDetailsService
+{
     private UserRepository userRepository;
     private Validator validator;
+    private VATClient vatClient;
 
     /**
      * Public constructor
@@ -37,11 +40,12 @@ public class UserService implements UserDetailsService {
      * @param validator      the user validator that the service needs to use
      */
     @Autowired
-    public UserService(UserRepository userRepository, Validator validator) {
+    public UserService(UserRepository userRepository, Validator validator)
+    {
         this.userRepository = userRepository;
         this.validator = validator;
+        this.vatClient = new VATClient();
     }
-
 
 
     /**
@@ -61,11 +65,11 @@ public class UserService implements UserDetailsService {
         userRepository.save(user);
     }
 
-/**
- * Find all users - <strong>Only for testing</strong>
- *
- * @return a list of all the users in the repository
- */
+    /**
+     * Find all users - <strong>Only for testing</strong>
+     *
+     * @return a list of all the users in the repository
+     */
     public List<User> findAllUsers()
     {
         return userRepository.findAll();
@@ -93,6 +97,10 @@ public class UserService implements UserDetailsService {
 
             throw new ConstraintViolationException("Error occurred: " + stringBuilder, violations);
         }
+
+        if (user instanceof Vendor vendor && vatClient.checkVatService(vendor.getVatNumber()) == null)
+            throw new InvalidDataException("VAT check failed");
+
         return userRepository.save(user);
     }
 
@@ -102,7 +110,8 @@ public class UserService implements UserDetailsService {
      * @param id the id of the user you want to find
      * @return the user with given id
      */
-    public User findUserById(long id) {
+    public User findUserById(long id)
+    {
         final User user = userRepository.findById(id).orElse(null);
         if (user == null) throw new NotFoundException(String.format("User with id %s not found", id));
         return user;
@@ -114,7 +123,8 @@ public class UserService implements UserDetailsService {
      * @param customerDto the customer dto object that needs to be converted
      * @return the customer object with the data from the dto
      */
-    public Customer toCustomer(CustomerDto customerDto) {
+    public Customer toCustomer(CustomerDto customerDto)
+    {
         Customer customer = new Customer();
 
         customer.setFirstName(customerDto.firstName());
@@ -131,7 +141,8 @@ public class UserService implements UserDetailsService {
      * @param customer the customer that needs to be converted
      * @return the customer dto object with the data from the dto object
      */
-    public CustomerDto toCustomerDto(Customer customer) {
+    public CustomerDto toCustomerDto(Customer customer)
+    {
         return new CustomerDto(customer.getFirstName(), customer.getLastName(), customer.getEmail(), customer.getPassword());
     }
 
@@ -144,10 +155,11 @@ public class UserService implements UserDetailsService {
     public Vendor save(VendorDto vendorDto)
     {
         if (findUserByEmail(vendorDto.email()) != null) throw new InvalidDataException("Email is not valid");
-        return userRepository.save(toVendor(vendorDto));
+        return (Vendor) this.save(toVendor(vendorDto));
     }
 
-    private Vendor toVendor(VendorDto vendorDto) {
+    private Vendor toVendor(VendorDto vendorDto)
+    {
         Vendor vendor = new Vendor();
         vendor.setIntroduction(vendorDto.introduction());
         vendor.setLogo(vendorDto.logo());
@@ -169,12 +181,14 @@ public class UserService implements UserDetailsService {
      * @param email the email of the user
      * @return the user with given email or null when not found
      */
-    public User findUserByEmail(String email) {
+    public User findUserByEmail(String email)
+    {
         return userRepository.findByEmail(email);
     }
 
     @Override
-    public UserDetails loadUserByUsername(String mail) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String mail) throws UsernameNotFoundException
+    {
         return userRepository.findByEmail(mail);
     }
 
@@ -184,7 +198,8 @@ public class UserService implements UserDetailsService {
      * @param vendorId the id of the vendor
      * @return the vendor
      */
-    public Vendor findVendorById(long vendorId) {
+    public Vendor findVendorById(long vendorId)
+    {
         User user = userRepository.findById(vendorId).orElse(null);
         if (user == null) throw new NotFoundException(String.format("User with id %s not found", vendorId));
         if (!(user instanceof Vendor))
@@ -198,7 +213,8 @@ public class UserService implements UserDetailsService {
      * @param vendorEmail the email of the vendor
      * @return the vendor from the repository
      */
-    public Vendor findVendorByEmail(String vendorEmail) {
+    public Vendor findVendorByEmail(String vendorEmail)
+    {
         User user = findUserByEmail(vendorEmail);
         if (user == null) throw new NotFoundException(String.format("User with email %s not found", vendorEmail));
         if (!(user instanceof Vendor vendor))
