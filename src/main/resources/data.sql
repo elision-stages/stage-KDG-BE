444c6ed5776e1b6f749590f9a04e6e50a876757c
vanbabe <104006124+vanbabe@users.noreply.github.com>
2022-05-27 19:30:13 +0200

    init admin on startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into admin (id, email, first_name, last_name, password, validated)
+values (1000, 'admin@elision.eu', 'Admin', 'Admin', '$2a$10$2Fwx2g5xnUWa.HeecMybv.SfDXO2wxjvyeuYx/9npkM0wxyPjTS26',
+        true);
