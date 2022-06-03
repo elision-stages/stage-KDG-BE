115d7aa1e7b43c66f9429687117fb03589087064
BertVanBaelen <104006124+vanbabe@users.noreply.github.com>
2022-06-03 16:25:49 +0200

    added testing

--- src/test/java/eu/elision/marketplace/logic/ControllerMockingTest.java
+++ src/test/java/eu/elision/marketplace/logic/ControllerMockingTest.java
@@ -3,15 +3,19 @@ package eu.elision.marketplace.logic;
 import eu.elision.marketplace.domain.product.Product;
 import eu.elision.marketplace.domain.product.category.Category;
 import eu.elision.marketplace.domain.product.category.attributes.value.DynamicAttributeValue;
+import eu.elision.marketplace.domain.users.Customer;
 import eu.elision.marketplace.domain.users.Vendor;
+import eu.elision.marketplace.logic.helpers.Mapper;
 import eu.elision.marketplace.logic.services.algolia.AlgoliaIndexerService;
 import eu.elision.marketplace.logic.services.product.DynamicAttributeService;
 import eu.elision.marketplace.logic.services.product.DynamicAttributeValueService;
 import eu.elision.marketplace.logic.services.product.ProductService;
 import eu.elision.marketplace.logic.services.users.UserService;
 import eu.elision.marketplace.web.dtos.attributes.AttributeValue;
+import eu.elision.marketplace.web.dtos.product.EditProductDto;
 import eu.elision.marketplace.web.dtos.product.ProductDto;
 import eu.elision.marketplace.web.webexceptions.InvalidDataException;
+import eu.elision.marketplace.web.webexceptions.NotFoundException;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
 import org.junit.jupiter.api.Test;
@@ -107,6 +111,7 @@ class ControllerMockingTest
         vendor.setId(RandomUtils.nextLong());
         vendor.setEmail(RandomStringUtils.randomAlphabetic(50));
         vendor.setBusinessName(RandomStringUtils.randomAlphabetic(50));
+        vendor.setToken(RandomStringUtils.randomAlphabetic(50));
 
         final ArrayList<String> images = new ArrayList<>();
         final ArrayList<AttributeValue<String, String>> attributes = new ArrayList<>();
@@ -180,5 +185,133 @@ class ControllerMockingTest
         assertThat(exception.getMessage()).isEqualTo("Token did not match vendor token");
     }
 
+    @Test
+    void testEditProductHappy()
+    {
+        final Vendor vendor = new Vendor();
+        vendor.setId(RandomUtils.nextLong());
+        vendor.setEmail(RandomStringUtils.randomAlphabetic(50));
+        vendor.setBusinessName(RandomStringUtils.randomAlphabetic(50));
+
+        final ArrayList<String> images = new ArrayList<>();
+        final ArrayList<AttributeValue<String, String>> attributes = new ArrayList<>();
+        final ArrayList<DynamicAttributeValue<?>> convertedAttribtues = new ArrayList<>();
+
+        final Category category = new Category();
+        category.setId(RandomUtils.nextLong());
+
+        Product product = new Product(
+                RandomUtils.nextLong(),
+                RandomUtils.nextInt(),
+                RandomStringUtils.randomAlphabetic(50),
+                category,
+                vendor,
+                RandomStringUtils.randomAlphabetic(50),
+                images,
+                convertedAttribtues
+        );
+
+        EditProductDto editProductDto = new EditProductDto(
+                0L,
+                category,
+                product.getTitle(),
+                product.getPrice(),
+                product.getDescription(),
+                images,
+                attributes
+        );
+        when(userService.findVendorByEmail(vendor.getEmail())).thenReturn(vendor);
+        when(dynamicAttributeService.getSavedAttributes(editProductDto.attributes(), category.getId())).thenReturn(convertedAttribtues);
+        when(productService.editProduct(Mapper.toProduct(editProductDto, editProductDto.category(), vendor, convertedAttribtues))).thenReturn(product);
+
+        assertThat(controller.editProduct(editProductDto, vendor.getEmail())).isEqualTo(product);
+    }
+
+    @Test
+    void testEditProductUserNotFound()
+    {
+        final Vendor vendor = new Vendor();
+        vendor.setId(RandomUtils.nextLong());
+        vendor.setEmail(RandomStringUtils.randomAlphabetic(50));
+        vendor.setBusinessName(RandomStringUtils.randomAlphabetic(50));
+
+        final ArrayList<String> images = new ArrayList<>();
+        final ArrayList<AttributeValue<String, String>> attributes = new ArrayList<>();
+        final ArrayList<DynamicAttributeValue<?>> convertedAttribtues = new ArrayList<>();
+
+        final Category category = new Category();
+        category.setId(RandomUtils.nextLong());
+
+        Product product = new Product(
+                RandomUtils.nextLong(),
+                RandomUtils.nextInt(),
+                RandomStringUtils.randomAlphabetic(50),
+                category,
+                vendor,
+                RandomStringUtils.randomAlphabetic(50),
+                images,
+                convertedAttribtues
+        );
+        EditProductDto editProductDto = new EditProductDto(
+                0L,
+                category,
+                product.getTitle(),
+                product.getPrice(),
+                product.getDescription(),
+                images,
+                attributes
+        );
+
+        when(userService.findUserByEmail(vendor.getEmail())).thenReturn(null);
+
+        final String vendorEmail = vendor.getEmail();
+        Exception exception = assertThrows(NotFoundException.class, () -> controller.editProduct(editProductDto, vendorEmail));
+
+        assertThat(exception.getMessage()).isEqualTo(String.format("User with email %s not found", vendor.getEmail()));
+    }
+
+    @Test
+    void testEditProductUserNotVendor()
+    {
+        final Vendor vendor = new Vendor();
+        vendor.setId(RandomUtils.nextLong());
+        vendor.setEmail(RandomStringUtils.randomAlphabetic(50));
+        vendor.setBusinessName(RandomStringUtils.randomAlphabetic(50));
+
+        final ArrayList<String> images = new ArrayList<>();
+        final ArrayList<AttributeValue<String, String>> attributes = new ArrayList<>();
+        final ArrayList<DynamicAttributeValue<?>> convertedAttribtues = new ArrayList<>();
+
+        final Category category = new Category();
+        category.setId(RandomUtils.nextLong());
+
+        Product product = new Product(
+                RandomUtils.nextLong(),
+                RandomUtils.nextInt(),
+                RandomStringUtils.randomAlphabetic(50),
+                category,
+                vendor,
+                RandomStringUtils.randomAlphabetic(50),
+                images,
+                convertedAttribtues
+        );
+        EditProductDto editProductDto = new EditProductDto(
+                0L,
+                category,
+                product.getTitle(),
+                product.getPrice(),
+                product.getDescription(),
+                images,
+                attributes
+        );
+
+        when(userService.findUserByEmail(vendor.getEmail())).thenReturn(new Customer());
+
+        final String vendorEmail = vendor.getEmail();
+        Exception exception = assertThrows(NotFoundException.class, () -> controller.editProduct(editProductDto, vendorEmail));
+
+        assertThat(exception.getMessage()).isEqualTo(String.format("User with email %s is not a vendor", vendor.getEmail()));
+
+    }
 
 }
